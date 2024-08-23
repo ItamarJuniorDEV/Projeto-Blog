2fdf0d276a6f677dda1d7189b4bd16d8147df612
Itamar Junior <cdajuniorf@gmail.com>
2024-08-23 17:56:26 -0300

    primeiro commit

---
+++ helpers/url.php
@@ -0,0 +1,3 @@
+<?php 
+$BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\') . '/';
+?>
---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <p>Hora de Codar &copy; 2020</p>
+</footer>
+
+
+
+
+
+</body>
+</html>
