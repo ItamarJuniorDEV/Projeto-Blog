3e6b8ccc42a7269772d2cca04ff02f78d7795c83
Itamar Junior <cdajuniorf@gmail.com>
2024-08-24 19:41:28 -0300

    Fiz o CSS do cabeçalho e rodaé e resgatei dados dos posts

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,7 @@
 <?php 
   include_once("helpers/url.php");
+  include_once("data/posts.php");
+  include_once("data/categories.php");
 ?>
 
 <!DOCTYPE html>
@@ -16,15 +18,15 @@
 </head>
 <body>
   <header>
-    <a href="<?= $BASE_URL ?>">
+    <a href="<?= $BASE_URL ?>" id="logo">
       <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Blog Codar">
     </a>
     <nav>
       <ul id="navbar">
-        <li><a href="<?= $BASE_URL ?>">Home</a></li>
-        <li><a href="#">Categorias</a></li>
-        <li><a href="#">Sobre</a></li>
-        <li><a href="<?= $BASE_URL ?>contato.php">Contato</a></li>
+        <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+        <li><a href="#" class="nav-link">Categorias</a></li>
+        <li><a href="#" class="nav-link">Sobre</a></li>
+        <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
       </ul>
     </nav>
   </header>
